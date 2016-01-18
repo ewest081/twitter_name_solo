d411e6cf54bc2e3e648b72a3d1019098045a8ce2
Liz Westberg <ewest081@gmail.com>
2016-01-18 15:36:49 -0600

    Yahtzee. It works now. Still needs styling

--- client/client.js
+++ client/client.js
@@ -10,35 +10,31 @@ var userArray = [];
 app.controller('MainController',['$scope','$http', function($scope, $http){
 
     $scope.getWords = function() {
-
-        fetchWords();
-
-        function fetchWords() {
-            $http.get('/getNouns').then(function (response) {
-                for (i = 0; i < response.data.nouns.length; i++) {
-                    nounArray.push(response.data.nouns[randomNumber(0, response.data.nouns.length - 1)].Noun);
-                }
-            });
-
-            $http.get('/getAdj').then(function (response) {
-                for (i = 0; i < response.data.adjectives.length; i++) {
-                    adjArray.push(response.data.adjectives[randomNumber(0, response.data.adjectives.length - 1)].Adjective);
-                }
-            });
-            makeUser();
-        }
-
-        function makeUser() {
             for (i = 0; i < nounArray.length; i++) {
                 userArray.push(adjArray[i] + nounArray[i])
             }
-        }
-
-        $scope.twitterPost = userArray;
+            $scope.twitterPost = userArray;
     };
+
+    function fetchWords() {
+        $http.get('/getNouns').then(function (response) {
+            for (i = 0; i < response.data.nouns.length; i++) {
+                nounArray.push(response.data.nouns[randomNumber(0, response.data.nouns.length - 1)].Noun);
+            }
+        });
+
+        $http.get('/getAdj').then(function (response) {
+            for (i = 0; i < response.data.adjectives.length; i++) {
+                adjArray.push(response.data.adjectives[randomNumber(0, response.data.adjectives.length - 1)].Adjective);
+            }
+        });
+    }
 
     function randomNumber(min, max) {
         return Math.floor(Math.random() * (1 + max - min) + min);
     }
+
+    fetchWords();
+
 }]);
 
